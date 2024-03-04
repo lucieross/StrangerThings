fc5b2f8bc03eded1dd8072f8e7d9dd0f3e9d962a
lucieross <146110204+lucieross@users.noreply.github.com>
2024-03-04 16:17:47 +0000

    Update CGrid.py

--- CGrid.py
+++ CGrid.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 fig, ax = plt.subplots()
 
-ax.set_xlim([0,150])
+ax.set_xlim([0,150]) #Creates Line
 ax.set_ylim([0,150])
-
 ax.plot([0, 150], [100, 100])
-ax.grid(visible=True)
+
+xpoints = np.array([75]) #Creates Placeholder for cannon
+ypoints = np.array([100])
+plt.plot(xpoints, ypoints, 's')
+
+ax.grid(visible=True) #Creates Grid Lines
 plt.show()
